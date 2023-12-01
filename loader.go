6a5450201da566c77ca5187def68bd6b4f882803
Lv Yonghuan <1914128278@qq.com>
2023-12-01 10:04:41 +0800

    panic recover

--- loader.go
+++ loader.go
@@ -30,7 +30,7 @@ func (dll *DllPackage) GetID() int {
 
 // GetFunctionsArgsTypes 获取函数传入参数类型
 // 传入：函数名
-// 传出：传入参数类型数组
+// 传出：传入参数类型数组,错误
 func (dll *DllPackage) GetFunctionsArgsTypes(methodName string) ([]string, error) {
 	functionArgs, isExist := dll.functionsArgsTypes[methodName]
 	if !isExist {
@@ -41,7 +41,7 @@ func (dll *DllPackage) GetFunctionsArgsTypes(methodName string) ([]string, error
 
 // GetFunctionReturnTypes 获得函数返回值类型列表
 // 传入：函数名
-// 传出：返回值类型列表
+// 传出：返回值类型列表,错误
 func (dll *DllPackage) GetFunctionReturnTypes(methodName string) ([]string, error) {
 	functionReturn, isExist := dll.functionsReturnTypes[methodName]
 	if !isExist {
@@ -59,8 +59,8 @@ func (dll *DllPackage) GetFunctions() []string {
 
 // GetInfo 获取别的信息
 // 传入：key
-// 传出：value
-func (dll *DllPackage) GetInfo(key string) (string, error) {
+// 传出：value,错误
+func (dll *DllPackage) GetInfo(key string) (info string, err error) {
 	info, isExist := dll.info[key]
 	if !isExist {
 		return "", util.NewError(_const.CommonException, _const.Bin, errors.New("info not exist"))
@@ -71,7 +71,15 @@ func (dll *DllPackage) GetInfo(key string) (string, error) {
 // Execute 执行函数
 // 传入：方法名，参数
 // 传出：返回值（通过指针）,错误
-func (dll *DllPackage) Execute(method string, args []uintptr, re uintptr) error {
+func (dll *DllPackage) Execute(method string, args []uintptr, re uintptr) (err error) {
+	//捕获恐慌
+	defer func() {
+		if er := recover(); er != nil {
+			//特例panic,级别非fatal,牵涉到cgo
+			err = util.NewError(_const.CommonException, _const.Bin, errors.New(er.(string)))
+		}
+	}()
+
 	// 在dll中获得方法的句柄
 	proc, err := dll.dll.FindProc(method)
 	if err != nil {
@@ -91,7 +99,15 @@ func (dll *DllPackage) Execute(method string, args []uintptr, re uintptr) error 
 // LoadBinPackage 根据路径加载二进制包并返回句柄
 // 传入：路径
 // 传出：二进制执行包
-func (dllLoader *DllLoader) LoadBinPackage(path string) (int, error) {
+func (dllLoader *DllLoader) LoadBinPackage(path string) (id int, err error) {
+	//捕获恐慌
+	defer func() {
+		if er := recover(); er != nil {
+			//特例panic,级别非fatal,牵涉到cgo
+			err = util.NewError(_const.CommonException, _const.Bin, errors.New(er.(string)))
+		}
+	}()
+
 	// dll包对应的描述文件地址
 	dllInfoPath := path + ".json"
 	// dll包地址
@@ -148,14 +164,25 @@ func (dllLoader *DllLoader) LoadBinPackage(path string) (int, error) {
 }
 
 // ReleasePackage 释放dll包
-// 传入：二进制执行包
-// 传出：无
-func (dllLoader *DllLoader) ReleasePackage(name string, id int) error {
+// 传入：包名，包id
+// 传出：错误
+func (dllLoader *DllLoader) ReleasePackage(name string, id int) (err error) {
+	//捕获恐慌
+	defer func() {
+		if er := recover(); er != nil {
+			//特例panic,级别非fatal,牵涉到cgo
+			err = util.NewError(_const.CommonException, _const.Bin, errors.New(er.(string)))
+		}
+	}()
+
 	//通过name和id获取dll package
-	dllPackage := dllLoader.Dlls[name][id]
+	dllPackage, isExist := dllLoader.Dlls[name][id]
+	if !isExist {
+		return util.NewError(_const.CommonException, _const.Bin, errors.New("package not exist"))
+	}
 
 	//对dll执行释放函数
-	err := dllPackage.Execute("Release", nil, 0)
+	err = dllPackage.Execute("Release", nil, 0)
 	if err != nil {
 		return util.NewError(_const.CommonException, _const.Bin, err)
 	}
